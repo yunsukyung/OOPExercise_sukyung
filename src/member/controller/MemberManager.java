0ac91c31df9c0151f2cf51ff338b069ce54f6626
s <s@sec>
2020-03-24 00:09:08 +0900

    수정하기
    패스워드, 이름, 이메일 수정하기 작성 완료

--- src/member/controller/MemberManager.java
+++ src/member/controller/MemberManager.java
@@ -8,7 +8,7 @@ public class MemberManager {
 	private static Member[] m = new Member[10];
 	private static int ctn = 0;
 	private Scanner sc = new Scanner(System.in);
-	
+
 
 	public void insertMember() {
 		for(int i = 0; i < m.length; i++) {
@@ -25,20 +25,20 @@ public class MemberManager {
 			char gender = sc.next().charAt(0);
 			System.out.println("E-mail : ");
 			String email = sc.next();
-			
+
 			m[i] = new Member(id, pwd, name, age, gender, email);
-			
+
 			ctn++;
 		}
 		System.out.println("입력이 완료되었습니다. 메인 메뉴로 돌아갑니다."); return;
-		
-		
-		
+
+
+
 	}
 
 	public void printAllMember() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void searchId() {
@@ -51,7 +51,7 @@ public class MemberManager {
 				System.out.println("검색한 회원 정보가 존재하지 않습니다."); break;
 			}
 		}
-		
+
 	}
 
 	public void searchName() {
@@ -67,33 +67,75 @@ public class MemberManager {
 	}
 
 	public void searchEmail() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("검색할 이메일을 입력하세요 : ");
+		String email = sc.next();
+		for(int i = 0; i < ctn; i++) {
+			if(m[i].getEmail() == email) {
+				System.out.println(m[i].printOne());
+			} else {
+				System.out.println("검색한 회원 정보가 존재하지 않습니다."); break;
+			}
+		}
+
 	}
 
 	public void updatePwd() {
-		// TODO Auto-generated method stub
+		System.out.println("수정할 회원의 아이디를 입력하세요 : ");
+		String userId = sc.next();
+		for(int i = 0; i < ctn; i++) {
+			if(m[i].getUserId() == userId) {
+				System.out.println("변경할 비밀번호를 입력하세요 : ");
+				String newPwd = sc.next();
+				m[i].setUserPwd(newPwd);
+				System.out.println("패스워드 수정이 완료되었습니다."); break;
+			} else {
+				System.out.println("수정할 회원이 존재하지 않습니다."); break;
+			}
+		}
 		
+
 	}
 
 	public void updateName() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("수정할 회원의 아이디를 입력하세요 : ");
+		String userId = sc.next();
+		for(int i = 0; i < ctn; i++) {
+			if(m[i].getUserId() == userId) {
+				System.out.println("변경할 이름을 입력하세요 : ");
+				String newName = sc.next();
+				m[i].setUserName(newName);;
+				System.out.println("이름 수정이 완료되었습니다."); break;
+			} else {
+				System.out.println("수정할 회원이 존재하지 않습니다."); break;
+			}
+		}
+
 	}
 
 	public void updateEmail() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("수정할 회원의 아이디를 입력하세요 : ");
+		String userId = sc.next();
+		for(int i = 0; i < ctn; i++) {
+			if(m[i].getUserId() == userId) {
+				System.out.println("변경할 이메일 입력하세요 : ");
+				String newEmail = sc.next();
+				m[i].setEmail(newEmail);;
+				System.out.println("이메일 수정이 완료되었습니다."); break;
+			} else {
+				System.out.println("수정할 회원이 존재하지 않습니다."); break;
+			}
+		}
+
 	}
 
 	public void deleteOne() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void deleteAll() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 
